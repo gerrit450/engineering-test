9a1acca7c58e396291c6486d3a35d79322259231
gerrit450 <gerrit2209@gmail.com>
2026-05-09 19:02:16 +1200

    test: Add Backstage passes quality update tests
    - Verify quality increases by 1 when SellIn is more than 10 days
    - Verify quality increases by 2 when SellIn is 10 days or less
    - Verify quality increases by 3 when SellIn is 5 days or less
    - Verify quality drops to 0 when past due date

---
+++ src/GildedRose.Tests/BackstageItemTests.cs
@@ -0,0 +1,68 @@
+﻿using GildedRose.Console;
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    public class BackstageItemTests
+    {
+        [Fact]
+        public void GivenBackstageItemAndSellinIsMoreThanTenDays_WhenCheckingQuality_ThenQualityShouldIncreaseOnce()
+        {
+            // arrange
+            var item_list = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = 0 } };
+            const int ExpectedQualityResult = 1;
+
+            // act
+            var app = new QualityControl();
+            app.UpdateQuality(item_list);
+
+            // assert
+            Assert.Equal(ExpectedQualityResult, item_list[0].Quality);
+        }
+
+        [Fact]
+        public void GivenBackstageItemAndSellinIsLessThanTenDays_WhenCheckingQuality_ThenQualityShouldIncreaseTwice()
+        {
+            // arrange
+            var item_list = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 9, Quality = 0 } };
+            const int ExpectedQualityResult = 2;
+
+            // act
+            var app = new QualityControl();
+            app.UpdateQuality(item_list);
+
+            // assert
+            Assert.Equal(ExpectedQualityResult, item_list[0].Quality);
+        }
+
+        [Fact]
+        public void GivenBackstageItemAndSellinIsLessThanFiveDays_WhenCheckingQuality_ThenQualityShouldIncreaseByThree()
+        {
+            // arrange
+            var item_list = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 4, Quality = 0 } };
+            const int ExpectedQualityResult = 3;
+
+            // act
+            var app = new QualityControl();
+            app.UpdateQuality(item_list);
+
+            // assert
+            Assert.Equal(ExpectedQualityResult, item_list[0].Quality);
+        }
+
+        [Fact]
+        public void GivenBackstageItemAndIsPastDueDate_WhenCheckingQuality_ThenQualityShouldEqualToZero()
+        {
+            // arrange
+            var item_list = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -2, Quality = 0 } };
+            const int ExpectedQualityResult = 0;
+
+            // act
+            var app = new QualityControl();
+            app.UpdateQuality(item_list);
+
+            // assert
+            Assert.Equal(ExpectedQualityResult, item_list[0].Quality);
+        }
+    }
+}
