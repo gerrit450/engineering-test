aeb2bf797f99d12efb1df4172da97f0f59c47252
gerrit450 <gerrit2209@gmail.com>
2026-05-10 09:20:25 +1200

    Refactor: * Added new ItemFactoryTests to ensure right ItemStrategy is returned   based on item name. Will change strings to enums to   ensure names are consistent when using ItemFactory. * Refactored unit tests to test the Item Strategies themselves. * Renamed unit tests to reflect this.

---
+++ src/GildedRose.Tests/LegendaryItemStrategyTests.cs
@@ -0,0 +1,33 @@
+﻿using GildedRose.Console;
+using GildedRose.Console.Strategies;
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    public class LegendaryItemStrategyTests
+    {
+        private readonly IQualityUpdateStrategy legendaryItemStrategy = new LegendaryItemStrategy();
+
+        [Fact]
+        public void GivenLegendaryItem_WhenCheckingQuality_ThenQualityShouldNeverDecrease()
+        {
+            var legendaryItem = new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 1, Quality = 80 };
+            const int ExpectedQualityResult = 80;
+
+            legendaryItemStrategy.Update(legendaryItem);
+
+            Assert.Equal(ExpectedQualityResult, legendaryItem.Quality);
+        }
+
+        [Fact]
+        public void GivenLegendaryItem_WhenCheckingQuality_ThenSellPastDateShouldNeverDecrease()
+        {
+            var legendaryItem = new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 1, Quality = 80 };
+            const int ExpectedSellinResult = 1;
+
+            legendaryItemStrategy.Update(legendaryItem);
+
+            Assert.Equal(ExpectedSellinResult, legendaryItem.SellIn);
+        }
+    }
+}
