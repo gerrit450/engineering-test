c37bc7a31e9883094ee1768506c0ca28f53fd984
gerrit450 <gerrit2209@gmail.com>
2026-05-09 16:44:29 +1200

    Refactor: Moved UpdateQuality method from Program.cs to a new class called QualityControl. The purpose of this refactor is to seperate the business logic into its own domain. This would allow easier readability and also the ability to create unit test for the rules based on the updateQuality method.

---
+++ src/GildedRose.Console/QualityControl.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GildedRose.Console.Program;
+
+namespace GildedRose.Console
+{
+    public class QualityControl
+    {
+        public void UpdateQuality(IList<Item> listOfItems)
+        {
+            for (var i = 0; i < listOfItems.Count; i++)
+            {
+                if (listOfItems[i].Name != "Aged Brie" && listOfItems[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (listOfItems[i].Quality > 0)
+                    {
+                        if (listOfItems[i].Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            listOfItems[i].Quality = listOfItems[i].Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if (listOfItems[i].Quality < 50)
+                    {
+                        listOfItems[i].Quality = listOfItems[i].Quality + 1;
+
+                        if (listOfItems[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (listOfItems[i].SellIn < 11)
+                            {
+                                if (listOfItems[i].Quality < 50)
+                                {
+                                    listOfItems[i].Quality = listOfItems[i].Quality + 1;
+                                }
+                            }
+
+                            if (listOfItems[i].SellIn < 6)
+                            {
+                                if (listOfItems[i].Quality < 50)
+                                {
+                                    listOfItems[i].Quality = listOfItems[i].Quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (listOfItems[i].Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    listOfItems[i].SellIn = listOfItems[i].SellIn - 1;
+                }
+
+                if (listOfItems[i].SellIn < 0)
+                {
+                    if (listOfItems[i].Name != "Aged Brie")
+                    {
+                        if (listOfItems[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (listOfItems[i].Quality > 0)
+                            {
+                                if (listOfItems[i].Name != "Sulfuras, Hand of Ragnaros")
+                                {
+                                    listOfItems[i].Quality = listOfItems[i].Quality - 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            listOfItems[i].Quality = listOfItems[i].Quality - listOfItems[i].Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (listOfItems[i].Quality < 50)
+                        {
+                            listOfItems[i].Quality = listOfItems[i].Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
