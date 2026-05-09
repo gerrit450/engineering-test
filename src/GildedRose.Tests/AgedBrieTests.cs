a873a4e32c46c2aa37b44d39f25fbbda95e27ded
gerrit450 <gerrit2209@gmail.com>
2026-05-09 18:53:19 +1200

    test: Add normal item And Aged Brie unit test covering:
    - Verify quality does not go below 0
    - Verify quality decreases twice as fast when past sell-by date
    - Verify SellIn value decreases by 1 each update
    
    - Verify quality caps at 50
    - Verify quality increases by 2 when past sell-by date
    - Verify quality increases by 1 before sell-by date

---
+++ src/GildedRose.Tests/AgedBrieTests.cs
@@ -0,0 +1,54 @@
+﻿using GildedRose.Console;
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    public class AgedBrieTests
+    {
+
+        [Fact]
+        public void GivenAgedBrieAndQualityIsFifty_WhenCheckingQuality_ThenQualityShouldNotGoAboveFifty()
+        {
+            // arrange
+            var item_list = new List<Item> { new Item { Name = "Aged Brie", SellIn = 1, Quality = 50 } };
+            const int ExpectedQualityResult = 50;
+
+            // act
+            var app = new QualityControl();
+            app.UpdateQuality(item_list);
+
+            // assert
+            Assert.Equal(ExpectedQualityResult, item_list[0].Quality);
+        }
+
+        [Fact]
+        public void GivenAgedBrieAndPastByDate_WhenCheckingQuality_ThenQualityShouldIncreaseByTwo()
+        {
+            // arrange
+            var item_list = new List<Item> { new Item { Name = "Aged Brie", SellIn = -1, Quality = 0 } };
+            const int ExpectedQualityResult = 2;
+
+            // act
+            var app = new QualityControl();
+            app.UpdateQuality(item_list);
+
+            // assert
+            Assert.Equal(ExpectedQualityResult, item_list[0].Quality);
+        }
+
+        [Fact]
+        public void GivenAgedBrieItemAndSellinIsGreaterThanZero_WhenCheckingQuality_ThenQualityShouldIncreaseByOne()
+        {
+            // arrange
+            var item_list = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 } };
+            const int ExpectedQualityResult = 1;
+
+            // act
+            var app = new QualityControl();
+            app.UpdateQuality(item_list);
+
+            // assert
+            Assert.Equal(ExpectedQualityResult, item_list[0].Quality);
+        }
+    }
+}
