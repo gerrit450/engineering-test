007c3e121b2b229ce741ce5fee2e9d4e3cfad521
gerrit450 <gerrit2209@gmail.com>
2026-05-09 23:38:56 +1200

    refactor: Using the factory and design pattern combination, the QualityControl class takes each item from caller and gives it to the ItemFactory which will return the correct ItemStrategy. We can run the Update method for each strategy to run the core logic of each item

--- src/GildedRose.Console/QualityControl.cs
+++ src/GildedRose.Console/QualityControl.cs
@@ -1,87 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static GildedRose.Console.Program;
+﻿using GildedRose.Console.Strategies;
 
 namespace GildedRose.Console
 {
-    public class QualityControl
+    public static class QualityControl
     {
-        public void UpdateQuality(IList<Item> listOfItems)
+        public static void UpdateQuality(IList<Item> listOfItems)
         {
-            for (var i = 0; i < listOfItems.Count; i++)
+            foreach (var item in listOfItems)
             {
-                if (listOfItems[i].Name != "Aged Brie" && listOfItems[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (listOfItems[i].Quality > 0)
-                    {
-                        if (listOfItems[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            listOfItems[i].Quality = listOfItems[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (listOfItems[i].Quality < 50)
-                    {
-                        listOfItems[i].Quality = listOfItems[i].Quality + 1;
-
-                        if (listOfItems[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (listOfItems[i].SellIn < 11)
-                            {
-                                if (listOfItems[i].Quality < 50)
-                                {
-                                    listOfItems[i].Quality = listOfItems[i].Quality + 1;
-                                }
-                            }
-
-                            if (listOfItems[i].SellIn < 6)
-                            {
-                                if (listOfItems[i].Quality < 50)
-                                {
-                                    listOfItems[i].Quality = listOfItems[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (listOfItems[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    listOfItems[i].SellIn = listOfItems[i].SellIn - 1;
-                }
-
-                if (listOfItems[i].SellIn < 0)
-                {
-                    if (listOfItems[i].Name != "Aged Brie")
-                    {
-                        if (listOfItems[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (listOfItems[i].Quality > 0)
-                            {
-                                if (listOfItems[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    listOfItems[i].Quality = listOfItems[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            listOfItems[i].Quality = listOfItems[i].Quality - listOfItems[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (listOfItems[i].Quality < 50)
-                        {
-                            listOfItems[i].Quality = listOfItems[i].Quality + 1;
-                        }
-                    }
-                }
+                var itemStrategy = ItemFactory.Create(item);
+                itemStrategy.Update(item);
             }
         }
     }
