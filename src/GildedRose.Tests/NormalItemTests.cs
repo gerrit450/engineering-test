1acb8fb9996b15aa7157aa9ca74a647b40fea049
gerrit450 <gerrit2209@gmail.com>
2026-05-09 18:53:19 +1200

    test: Add normal item And Aged Brie unit test covering:
    - Verify quality does not go below 0
    - Verify quality decreases twice as fast when past sell-by date
    - Verify SellIn value decreases by 1 each update
    
    - Verify quality caps at 50
    - Verify quality increases by 2 when past sell-by date
    - Verify quality increases by 1 before sell-by date

---
+++ src/GildedRose.Tests/NormalItemTests.cs
@@ -0,0 +1,53 @@
+using GildedRose.Console;
+using Xunit;
+
+namespace GildedRose.Tests;
+
+public class NormalItemTests
+{
+
+    [Fact]
+    public void GivenNormalItemAndQualityIsZero_WhenCheckingQuality_ThenQualityShouldNotGoBelowZero()
+    {
+        // arrange
+        var item_list = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 1, Quality = 0 } };
+        const int ExpectedQualityResult = 0;
+
+        // act
+        var app = new QualityControl();
+        app.UpdateQuality(item_list);
+
+        // assert
+        Assert.Equal(ExpectedQualityResult, item_list[0].Quality);
+    }
+
+    [Fact]
+    public void GivenNormalItemAndSellinIsPastByDate_WhenCheckingQuality_ThenQualityShouldReduceTwiceAsFast()
+    {
+        // arrange
+        var item_list = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = -1, Quality = 10 } };
+        const int ExpectedQualityResult = 8;
+
+        // act
+        var app = new QualityControl();
+        app.UpdateQuality(item_list);
+
+        // assert
+        Assert.Equal(ExpectedQualityResult, item_list[0].Quality);
+    }
+
+    [Fact]
+    public void GivenNormalItem_WhenCheckingQuality_SellInValueShouldReducebyOne()
+    {
+        // arrange
+        var item_list = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 5, Quality = 0 } };
+        const int ExpectedSellInResult = 4;
+
+        // act
+        var app = new QualityControl();
+        app.UpdateQuality(item_list);
+
+        // assert
+        Assert.Equal(ExpectedSellInResult, item_list[0].SellIn);
+    }
+}
