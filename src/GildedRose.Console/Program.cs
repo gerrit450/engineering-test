803f4ae9bec9ae09667c7e2b5b95280f4fd01173
gerrit450 <gerrit2209@gmail.com>
2026-05-09 16:44:29 +1200

    Refactor: Moved UpdateQuality method from Program.cs to a new class called QualityControl. The purpose of this refactor is to seperate the business logic into its own domain. This would allow easier readability and also the ability to create unit test for the rules based on the updateQuality method.

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections.Generic;
-
-namespace GildedRose.Console;
+﻿namespace GildedRose.Console;
 
 public class Program
 {
     public IList<Item> Items = new List<Item>();
+    public QualityControl qualityControl = new QualityControl();
 
     static void Main(string[] args)
     {
         System.Console.WriteLine("OMGHAI!");
 
         var app = new Program()
-                      {
-                          Items = new List<Item>
+        {
+            Items = new List<Item>
                                       {
                                           new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                                           new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
@@ -27,88 +26,18 @@ public class Program
                                           new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
                                       }
 
-                      };
+        };
 
-        app.UpdateQuality();
 
+        app.Run();
         System.Console.ReadKey();
     }
 
-    public void UpdateQuality()
+    public void Run()
     {
-        for (var i = 0; i < Items.Count; i++)
-        {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (Items[i].Quality > 0)
-                {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (Items[i].Quality < 50)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
-
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-            {
-                Items[i].SellIn = Items[i].SellIn - 1;
-            }
-
-            if (Items[i].SellIn < 0)
-            {
-                if (Items[i].Name != "Aged Brie")
-                {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
-                }
-            }
-        }
+        qualityControl.UpdateQuality(Items);
     }
+
 }
 
 public class Item
