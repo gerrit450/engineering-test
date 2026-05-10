24a97fb7a7fc183eb0337c2b217fed034d9b5f70
gerrit450 <gerrit2209@gmail.com>
2026-05-10 12:35:57 +1200

    feature: Added ConjuredItemStrategy and unit tests
    For this change, I implemented the ConjuredItemStrategy that contains the
    strategic logic when updating the quality and sellin values.
    Also added unit tests to verify and test these requirements.

---
+++ src/GildedRose.Console/Strategies/ConjuredItemStrategy.cs
@@ -0,0 +1,13 @@
+﻿namespace GildedRose.Console.Strategies
+{
+    public class ConjuredItemStrategy : IQualityUpdateStrategy
+    {
+        public void Update(Item item)
+        {
+            item.Quality = item.SellIn < 0
+                ? Math.Max(0, item.Quality - 4)
+                : Math.Max(0, item.Quality - 2);
+            item.SellIn--;
+        }
+    }
+}
