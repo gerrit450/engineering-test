b790fa24521143243d39877bef8715235cecaa96
gerrit450 <gerrit2209@gmail.com>
2026-05-10 12:43:25 +1200

    Refactor: Added ConjuredItemStrategy to factory
    Added the ConjuredItemStrategy to the ItemFactory.

--- src/GildedRose.Console/ItemFactory.cs
+++ src/GildedRose.Console/ItemFactory.cs
@@ -5,6 +5,7 @@
         public static IQualityUpdateStrategy Create(Item item) => item.Name switch
         {
             "Aged Brie" => new AgedBrieStrategy(),
+            "Conjured Mana Cake" => new ConjuredItemStrategy(),
             "Backstage passes to a TAFKAL80ETC concert" => new BackstageItemStrategy(),
             "Sulfuras, Hand of Ragnaros" => new LegendaryItemStrategy(),
             _ => new NormalItemStrategy()
